696bbf685f5b9c771f98e8f82258f237d305525f
Fariafrin <fafrin613@gmail.com>
2025-07-04 02:44:13 +0600

    Navigate to add. employee screen

--- tests/Creteemployee.spec.ts
+++ tests/Creteemployee.spec.ts
@@ -16,15 +16,34 @@ test("OrangeHRM Login Test", async ({ page }) => {
     await page.getByRole("button", { name: "Login" }).click();
 
     // Assertion to ensure Login was successful
-    await expect(page).toHaveURL(/.*dashboard/);
+    await expect.soft(page).toHaveURL(/.*dashboard/);
 
     // Optionally, check for a Dashboard element
     const dashboardHeader = page.locator("h6:has-text('Dashboard')");
-    await expect(dashboardHeader).toBeVisible();
+    await expect.soft(dashboardHeader).toBeVisible();
 
     //await page.getByRole("link", { name: "PIM" }).click();
 
     //await expect(page).toHaveURL(/.*"PIM"/);
+    //await page.pause();
     await page.getByRole("link", { name: "PIM" }).click();
-    await expect(page).toHaveURL(/.*"PIM"/);
+    await expect.soft(page).toHaveURL(/.*pim.*/i);
+
+    const pimHeader = page.locator("h5:has-text('Employee Information')");
+    await expect.soft(pimHeader).toBeVisible();
+    // await page.pause();
+    // await page.getByPlaceholder("Type for hints..").fill("Lisa");
+    await page.getByRole("button", { name: " Add" }).click();
+    await expect.soft(page).toHaveURL(/.*pim.*/i);
+    const addempHeader = page.locator("h6:has-text('Add Employee')");
+    await expect.soft(addempHeader).toBeVisible();
+
+    await page.getByPlaceholder("First Name").fill("Lisa");
+    await page.getByPlaceholder("Middle Name").fill("AF");
+    await page.getByPlaceholder("Last Name").fill("RiN");
+
+    const toggleSelector = "input[type='checkbox']";
+    await page.click(toggleSelector);
+    await page.pause();
+    // console.log("*************************************************");
 });
