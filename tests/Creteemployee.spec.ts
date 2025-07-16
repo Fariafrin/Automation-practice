154cfbcd5a2a0eb5f96f0cc74ed92e33936e1993
Fariafrin <fafrin613@gmail.com>
2025-07-16 14:16:24 +0600

    Test files naming format

--- tests/Creteemployee.spec.ts
+++ tests/Creteemployee.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 import { login } from "../utilities/Login";
 import { createemployee } from "../utilities/createemployee";
 
